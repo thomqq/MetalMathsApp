14f93390714939b675432c1f90ba8a52196121b4
thomqq <thomqq@gmail.com>
2020-08-11 09:27:53 +0200

    Initial commit

---
+++ src/main/java/tq/arxsoft/metalmaths/MetalMathsApplication.java
@@ -0,0 +1,13 @@
+package tq.arxsoft.metalmaths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetalMathsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetalMathsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tq/arxsoft/metalmaths/MetalMathsApplicationTests.java
@@ -0,0 +1,13 @@
+package tq.arxsoft.metalmaths;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetalMathsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
